48073aa55bda0c46133b36aec4d763a4a6a26f0f
emanuelemontini1998 <emanuele.montini1998@gmail.com>
2026-02-04 00:59:34 +0100

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,17 @@
+export const State = {
+    products: [],
+    sales: [],
+    waste: [],
+    cart: [],
+    filter: 'all',
+    tempQty: { id: null, val: '1', name: '', unit: '' },
+    cartExpanded: false
+};
+
+export const CATEGORIES = { 
+    beef: 'Manzo', 
+    pork: 'Maiale', 
+    poultry: 'Pollame', 
+    lamb: 'Agnello', 
+    processed: 'Lavorati' 
+};
